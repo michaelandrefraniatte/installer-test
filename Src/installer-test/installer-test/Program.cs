160b10e4400ff0645e693dc9dfb43ed9cf607ac7
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-04 02:47:41 +0200

    Initial commit

---
+++ Src/installer-test/installer-test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using IWshRuntimeLibrary;
+namespace installer_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string zipPath = Environment.CurrentDirectory + @"\test.zip";
+            string extractPath = @"c:\test";
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+            System.IO.File.Copy(Environment.CurrentDirectory + @"\auth.txt", @"c:\test\auth.txt");
+            CreateShortcut();
+            Environment.Exit(0);
+        }
+        private static void CreateShortcut()
+        {
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\test.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "Shortcut for a test";
+            shortcut.TargetPath = @"c:\test\test.exe";
+            shortcut.WorkingDirectory = @"c:\test";
+            shortcut.Save();
+        }
+    }
+}
---
+++ Src/uninstaller-test/uninstaller-test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace uninstaller_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\test";
+            System.IO.Directory.Delete(path, true);
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            System.IO.File.Delete(System.IO.Path.Combine(desktopPath, "test.lnk"));
+            Environment.Exit(0);
+        }
+    }
+}
