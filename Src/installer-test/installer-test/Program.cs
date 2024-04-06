a83fd1ba734c95d6b8248c4352eee42e31299d39
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-07 01:32:35 +0200

    Add about

--- Src/installer-test/installer-test/Program.cs
+++ Src/installer-test/installer-test/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using IWshRuntimeLibrary;
+using System.Windows.Forms;
+
 namespace installer_test
 {
     class Program
     {
+        static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
         static void Main(string[] args)
         {
             string zipPath = Environment.CurrentDirectory + @"\test.zip";
