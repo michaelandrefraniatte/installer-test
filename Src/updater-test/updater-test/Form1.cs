c524c5fc307ad8397b973be1bccc58673904c80b
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-21 22:27:12 +0200

    Add a program

---
+++ Src/updater-test/updater-test/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updater_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string path = textBox2.Text;
+            if (!File.Exists(path))
+                SaveFile(url, path);
+            if (File.Exists(path))
+                Process.Start(path);
+        }
+        public void SaveFile(string url, string filename)
+        {
+            WebClient client = new WebClient();
+            client.DownloadFile(url, filename);
+            client.Dispose();
+        }
+    }
+}
